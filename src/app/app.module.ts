84e620878602717df48409b4d068d2176a73c618
majhaj <maja.hajda@gmail.com>
2023-05-18 15:45:02 +0200

    Some changes from previous lessons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,11 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
 import { FormsModule } from '@angular/forms';
+import { DropdownDirective } from './shared/dropdown.directive';
+
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -20,13 +24,14 @@ import { FormsModule } from '@angular/forms';
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,39 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe(
+            'Burger',
+            'A super tasty burger.',
+            'https://www.alemeksyk.eu/images/blog/tully_and_burger/TB_Marriott-2.jpg',
+            [
+                new Ingredient('Burger meat', 1),
+                new Ingredient('Buns', 2),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            'Chili con carne',
+            'Delicious chili!',
+            'https://live.staticflickr.com/2547/3862672238_30d378e7d6.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('Bean', 1)
+            ])
+    ];
+
+    constructor(private shoppingListService: ShoppingListService)  {}
+
+      getRecipes() {
+        return this.recipes.slice();
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
+      }
+}
